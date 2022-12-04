05d25043cef290dd0c8af385faa75282c5bfd766
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-04 19:18:30 +0300

    fix Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,43 +1,32 @@
 package hexlet.code.games;
 
-import hexlet.code.Engine;
 import  java.util.Random;
-
 public class Prime {
-    private static final String DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-    public static void startGame() {
+    public static final String GAME_TASK = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    public static boolean isPrime(int number) {
 
-        String[][] gameData = new String[Engine.COUNT_OF_ROUNDS][2];
-
-        for (int i = 0; i < gameData.length; i++) {
-            gameData[i] = prime();
-        }
-        System.out.println(Engine.checkResult(DESCRIPTION, gameData));
-    }
-    public static String[] prime() {
-
-        Random rand = new Random();
-
-        int number = rand.nextInt(100 - 1) + 1;
         int i = 2;
-        boolean isPrime = true;
-        String correct = "";
 
         while (i < Math.ceil((float) number / 2)) {
             if (number % i != 0) {
                 i++;
                 continue;
             }
-            isPrime = false;
-            break;
+            return false;
         }
+        return true;
+    }
+    public static String[] getQAPair() {
+
+        Random rand = new Random();
+
+        int number = rand.nextInt(100 - 1) + 1;
 
         String question = Integer.toString(number);
-        String correctAnswer = isPrime ? "yes" : "no";
+        String correctAnswer = isPrime(number) ? "yes" : "no";
 
-        String[] oneRound = {question, correctAnswer};
+        return new String[]{question, correctAnswer};
 
-        return oneRound;
     }
 }
 
