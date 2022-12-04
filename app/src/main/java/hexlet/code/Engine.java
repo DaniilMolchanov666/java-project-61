baec8fe9493f862de9b9d88f17043290dfd47aee
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-04 19:19:32 +0300

    add new method and fix Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,16 +5,22 @@ public class Engine {
     public static final int QUESTION = 0;
     public static final int CORRECT_ANSWER = 1;
     public static final int COUNT_OF_ROUNDS = 3;
-    public static String checkResult(String description, String[][] questions) {
 
-        System.out.println(description);
+    public static String[][] getGameData(String[] pairOne, String[] pairTwo, String[] pairThree) {
+
+        return new String[][]{pairOne, pairTwo, pairThree};
+    }
+
+    public static String checkResult(String gameTask, String[][] questionAnswerPairs) {
+
+        System.out.println(gameTask);
 
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
 
-            String question = questions[i][Engine.QUESTION];
-            String correctAnswer = questions[i][Engine.CORRECT_ANSWER];
+            String question = questionAnswerPairs[i][Engine.QUESTION];
+            String correctAnswer = questionAnswerPairs[i][Engine.CORRECT_ANSWER];
 
             System.out.println("Question: " + question);
             System.out.println("Your answer: ");
