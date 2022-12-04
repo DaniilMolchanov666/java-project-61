b22207329eb336ea87e4e9cd627816852c8bb321
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-04 19:18:00 +0300

    fix Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,37 +1,23 @@
 package hexlet.code.games;
 
-import hexlet.code.Engine;
 import java.util.Random;
 public class Even {
-    private static final String DESCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-    public static void startGame() {
 
-        String[][] gameData = new String[Engine.COUNT_OF_ROUNDS][2];
+    public static final String GAME_TASK = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    public static boolean isEven(int number) {
 
-        for (int i = 0; i < gameData.length; i++) {
-            gameData[i] = even();
-        }
-        System.out.println(Engine.checkResult(DESCRIPTION, gameData));
+        return number % 2 == 0;
     }
-
-    public static String[] even() {
+    public static String[] getQAPair() {
 
         Random rand = new Random();
 
         int number = rand.nextInt(100 - 1) + 1;
 
-        String correctAnswer = "";
-
-        if (number % 2 == 0) {
-            correctAnswer = "yes";
-        } else {
-            correctAnswer = "no";
-        }
+        String correctAnswer = isEven(number) ? "yes" : "no";
 
         String question = Integer.toString(number);
 
-        String[] oneRound = {question, correctAnswer};
-
-        return oneRound;
+        return new String[]{question, correctAnswer};
     }
 }
