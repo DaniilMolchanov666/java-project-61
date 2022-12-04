14760837168924bb79616f98807065653bec4fa3
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-04 19:17:35 +0300

    fix Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,42 +1,34 @@
 package hexlet.code.games;
 
-import hexlet.code.Engine;
 import java.util.Random;
 public class Calc {
-    private static final String DESCRIPTION = "Find the greatest common divisor of given numbers.";
-    public static void startGame() {
 
-        String[][] gameData = new String[Engine.COUNT_OF_ROUNDS][2];
-
-        for (int i = 0; i < gameData.length; i++) {
-            gameData[i] = calc();
-        }
-        System.out.println(Engine.checkResult(DESCRIPTION, gameData));
-    }
-    public static String[] calc() {
+    public static final String GAME_TASK = "What is the result of the expression?";
+    private static final String[] MATH_OPERATIONS = {"+", "-", "*"};
+    public static String[] getQAPair() {
 
         Random r = new Random();
 
-        int correct = 0;
-        String symbols = "+-*";
-        char c = symbols.charAt(r.nextInt(symbols.length()));
+        int random = r.nextInt(MATH_OPERATIONS.length);
 
+        String operation = MATH_OPERATIONS[random];
+
+        int correct = 0;
         int numberOne = r.nextInt(50);
         int numberTwo = r.nextInt(50);
 
-        if (c == '+') {
+        if (operation.equals("+")) {
             correct = numberOne + numberTwo;
-        } else if (c == '-') {
+        } else if (operation.equals("-")) {
             correct = numberOne - numberTwo;
         } else {
             correct = numberOne * numberTwo;
         }
 
-        String question = "" + numberOne + c + numberTwo;
+        String question = numberOne + operation + numberTwo;
         String correctAnswer = Integer.toString(correct);
 
-        String[] oneRound = {question, correctAnswer};
-
-        return oneRound;
+        return new String[]{question, correctAnswer};
     }
+
 }
