90ce35223b22e1c79bc82c5479aba05bed374b41
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-08 12:24:32 +0300

    fix app/src/main/java/hexlet/code/games/Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,11 +1,26 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 public class Calc {
 
-    public static final String GAME_TASK = "What is the result of the expression?";
+    public static final String CALCULATE_GAME_TASK = "What is the result of the expression?";
     private static final String[] MATH_OPERATIONS = {"+", "-", "*"};
-    public static String[] getQAPair() {
+
+    public static void startCalculateGame() {
+
+        String[][] calculateGameData = new String[Engine.COUNT_OF_ROUNDS][2];
+
+        for (int i = 0; i < calculateGameData.length; i++) {
+
+            calculateGameData[i] = getAnswerQuestionPair();
+        }
+
+        System.out.println(Engine.checkResult(CALCULATE_GAME_TASK, calculateGameData));
+    }
+
+    public static String[] getAnswerQuestionPair() {
 
         Random r = new Random();
 
@@ -15,7 +30,7 @@ public class Calc {
 
         int correct = 0;
         int numberOne = r.nextInt(50);
-        int numberTwo = r.nextInt(50);
+        int numberTwo = r.nextInt(10);
 
         if (operation.equals("+")) {
             correct = numberOne + numberTwo;
