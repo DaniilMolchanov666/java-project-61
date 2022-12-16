c5e770fe033a13a679a852bcc3a862155013d6cf
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2022-12-16 17:59:56 +0300

    fix all last mistakes and name of values

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -25,7 +25,6 @@ public class Even {
                 + MIN_BOUND_OF_RANDOM_VALUE;
 
         String correctAnswer = isEven(randomNumber) ? ANSWER_YES : ANSWER_NO;
-
         String question = Integer.toString(randomNumber);
 
         return new String[]{question, correctAnswer};
